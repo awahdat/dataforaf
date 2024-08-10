c0d4e4764b609033a48dd5565828f3088a994431
awahdat <51273010+awahdat@users.noreply.github.com>
2024-08-10 00:20:00 -0400

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
-
+import icon from "astro-icon";
 import fs from "fs";
 import rehypeExternalLinks from "rehype-external-links";
 import remarkUnwrapImages from "remark-unwrap-images";
@@ -18,6 +18,7 @@ export default defineConfig({
 	},
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
+    icon(),
 		tailwind({
 			applyBaseStyles: false,
 			nesting: true,
