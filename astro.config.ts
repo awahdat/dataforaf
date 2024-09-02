5d78cc876c976d0788a9ac2d8f0e9fe5cbdef033
awahdat <51273010+awahdat@users.noreply.github.com>
2024-09-02 00:08:24 -0400

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://dataforaf.pages.dev/",
+	site: "https://dataforafghanistan.org/",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
